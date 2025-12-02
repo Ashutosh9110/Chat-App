399590e61bce2cb35870d96501d7d1abd2cbccfa
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-12-02 14:12:54 +0530

    implmented logout button and login page functionality

---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { supabase } from "../lib/supabase";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) setError(error.message);
+    else window.location.href = "/"; // redirect
+  };
+
+  return (
+    <div className="auth">
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button>Login</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </form>
+    </div>
+  );
+}
